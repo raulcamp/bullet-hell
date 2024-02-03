6321d6657fee33b880d8efdd390eab9e020ef064
Raul Campos <raulcamp@mit.edu>
2024-02-03 15:43:13 -0800

    added enemy spawning

--- src/main.py
+++ src/main.py
@@ -2,14 +2,17 @@
 import sys
 import random
 import pygame
-from entities import Player
+from entities import Player, Enemy
 from constants import (
     WIDTH,
     HEIGHT,
     PLAYER_WIDTH,
     PLAYER_HEIGHT,
+    ENEMY_WIDTH,
+    ENEMY_HEIGHT,
     BLACK,
-    WHITE
+    WHITE,
+    RED,
 )
 
 
@@ -19,23 +22,47 @@ class Game:
         self.width = width
         self.height = height
         self.player = Player(
-            PLAYER_WIDTH, PLAYER_HEIGHT, WHITE,
+            PLAYER_WIDTH, PLAYER_HEIGHT, RED,
             width // 2 - PLAYER_WIDTH // 2,
             height // 2 - PLAYER_HEIGHT // 2)
+        self.enemies = []
 
     def update(self):
         """Update game state"""
-        pass
+        for enemy in self.enemies:
+            enemy.update_pos()
+            if collision(enemy, self.player):
+                self.enemies.remove(enemy)
+        if not random.randrange(5):
+            self.add_enemy()
+
+    def add_enemy(self):
+        """Generates a new Enemy"""
+        if random.getrandbits(1):
+            x = random.randint(0 - ENEMY_WIDTH, self.width)
+            y = random.choice([0 - ENEMY_HEIGHT, self.height])
+        else:
+            x = random.choice([0 - ENEMY_WIDTH, self.width])
+            y = random.randint(0 - ENEMY_HEIGHT, self.height)
+        tx, ty = self.player.get_x(), self.player.get_y()
+        time = 200  # TODO: make constant
+        dx = (tx - x) / time
+        dy = (ty - y) / time
+        enemy = Enemy(ENEMY_WIDTH, ENEMY_HEIGHT, WHITE, x, y, dx, dy)
+        self.enemies.append(enemy)
+        return enemy
 
     def draw(self, screen):
         """Draw the current game objects"""
         draw_rect(screen, self.player)
+        for enemy in self.enemies:
+            draw_rect(screen, enemy)
 
 
 def collision(obj1, obj2):
     """Checks if two objects collide"""
-    obj1x, obj1y = obj1.get_pos()
-    obj2x, obj2y = obj2.get_pos()
+    obj1x, obj1y = obj1.get_x(), obj1.get_y()
+    obj2x, obj2y = obj2.get_x(), obj2.get_y()
     if obj1x < obj2x + obj2.width \
             and obj1x + obj1.width > obj2x:
         if obj1y < obj2y + obj2.height \
