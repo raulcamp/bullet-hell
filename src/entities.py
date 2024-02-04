a8a74795ac0bb67b00dcf679721b7af3d838c523
Raul Campos <raulcamp@mit.edu>
2024-02-03 23:27:48 -0800

    add health + healthbar classes

--- src/entities.py
+++ src/entities.py
@@ -2,9 +2,11 @@
 from dataclasses import dataclass, field
 import math
 from constants import (
+    WIDTH,
     PROJECTILE_WIDTH,
     PROJECTILE_HEIGHT,
     PROJECTILE_SPEED_FACTOR,
+    RED,
     GREEN,
 )
 
@@ -76,9 +78,67 @@ class Projectile(Object):
 
 
 @dataclass
+class Health:
+    """Represents a healthbar"""
+    width: int = 0.9*WIDTH
+    o_width: int = 0.9*WIDTH
+    height: int = 15
+    color: tuple = RED
+    x: int = 20
+    y: int = 20
+    hp: int = 100
+    max_hp: int = 100
+
+    def get_width(self):
+        """Returns the width of the healthbar"""
+        return self.width
+
+    def get_height(self):
+        """Returns the height of the healthbar"""
+        return self.height
+
+    def get_color(self):
+        """Returns the color of the healthbar"""
+        return self.color
+
+    def get_x(self):
+        """Returns the x-coordinate of the healthbar"""
+        return self.x
+
+    def get_y(self):
+        """Returns the y-coordinate of the healthbar"""
+        return self.y
+
+    def get_hp(self):
+        """Returns the hp of the healthbar"""
+        return self.hp
+
+    def damage(self, amount):
+        """Update the healthbar with amount of damage"""
+        self.hp -= amount
+        self.hp = max(self.hp, 0)
+        self.width = self.o_width * (self.hp / self.max_hp)
+
+
+@dataclass
 class Player(Object):
     """Represents a Player"""
     projectiles: list = field(default_factory=list)
+
+    def __post_init__(self):
+        self.health = Health()
+
+    def get_health(self):
+        """Gets the player's health"""
+        return self.health
+
+    def receive_damage(self, amount):
+        """Recieve damage for the player"""
+        self.get_health().damage(amount)
+
+    def is_dead(self):
+        """Check if the player is dead"""
+        return not self.get_health().get_hp()
 
     def get_projectiles(self):
         """Gets the player's fired projectiles"""
@@ -86,7 +146,7 @@ class Player(Object):
 
     def shoot_projectile(self, tx, ty):
         """Shoot a Projectile"""
-        x, y = self.get_x(), self.get_y()
+        x, y = self.get_x() + self.width // 2, self.get_y() + self.height // 2
         xv, yv = (tx - x), (ty - y)
         norm = math.sqrt(xv**2 + yv**2)
         dx = PROJECTILE_SPEED_FACTOR * xv / norm
